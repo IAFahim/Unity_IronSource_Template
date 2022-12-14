dab0d4a0d3d6d224407b2ea32f22b23994f94c6f
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-12-14 18:51:44 +0600

    Go up

--- Assets/DataBase/Auth/GoogleAuth.cs
+++ Assets/DataBase/Auth/GoogleAuth.cs
@@ -1,7 +1,50 @@
-﻿namespace DataBase.Auth
+﻿using System;
+using System.Threading.Tasks;
+using Firebase;
+using Google;
+using TMPro;
+using UnityEngine;
+
+namespace DataBase.Auth
 {
-    public class GoogleAuth
+    public class GoogleAuth : MonoBehaviour
     {
-        
+        public TextMeshProUGUI text;
+
+        private Firebase.DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+        private GoogleSignInConfiguration googleSignInConfiguration;
+        Firebase.Auth.FirebaseAuth auth;
+        Firebase.Auth.FirebaseUser user;
+
+        private void Awake()
+        {
+            googleSignInConfiguration = new GoogleSignInConfiguration
+            {
+                WebClientId = "557262155384-m95fvig6gq6alnfgsgppdt0f9fi0pprr.apps.googleusercontent.com",
+                RequestIdToken = true
+            };
+        }
+
+        private void Start()
+        {
+        }
+
+        private void InitFirebase()
+        {
+            auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        }
+
+        public void GoogleSign()
+        {
+            GoogleSignIn.Configuration = googleSignInConfiguration;
+            GoogleSignIn.Configuration.UseGameSignIn = false;
+            GoogleSignIn.Configuration.RequestIdToken = true;
+            GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished);
+        }
+
+        private void OnAuthenticationFinished(Task<GoogleSignInUser> obj)
+        {
+            text.text="OnAuthenticationFinished";
+        }
     }
 }
